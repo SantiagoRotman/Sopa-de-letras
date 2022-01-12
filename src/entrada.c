5e3d269e3250ba4d12ce7c2527d17be1d50e4b5f
SantiagoRotman <SantiagoNicolasRotman@gmail.com>
2022-01-11 23:47:44 -0300

    entrada.c: Guardo el lemario

--- src/entrada.c
+++ src/entrada.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#define MAX_WORDS 1000000
+#define PATH "/home/santi/escuela/Sopa-de-letras/docs/lemario.txt" //temporal
 
 int arg_check(int argc, char **argv);
+float diagonal(char* lado);
+
 
 int main(int argc, char **argv){
-	FILE* fp;
-	char buff[255];
 	arg_check(argc, argv);
+	char  buff[255];
+    char* lemario[MAX_WORDS];
 
-	fp = fopen(argv[1], "r");
+	FILE *in, *out;
+    in = fopen(PATH, "r+");
+    out = fopen("out.txt", "w");
 
-	for(int i = 0; fscanf(fp, "%[^\n] ", buff) != EOF; i++ ){
-		printf("%s\n", buff);
+	for(int i = 0; fscanf(in, "%[^\n] ", buff) != EOF; i++ ){
+		if(strlen(buff)-1 <= diagonal(argv[2])){
+			lemario[i] = malloc(1+strlen(buff)*sizeof(char));
+			strcpy(lemario[i], buff);
+			printf("%s\n", lemario[i]);
+		}
+		else i--;
 	}
-
 }
 
 
@@ -24,4 +36,8 @@ int arg_check(int argc, char **argv){
 	if(sizeof(argv[3]) != sizeof(int))   return 0;
 	if(sizeof(argv[4]) != sizeof(int))   return 0;
 	return 1;
+}
+
+float diagonal(char* lado){
+	return sqrt(2*pow(atoi(lado), 2));
 }
